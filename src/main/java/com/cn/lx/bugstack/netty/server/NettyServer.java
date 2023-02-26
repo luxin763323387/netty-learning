ad78431ff16c9507efd1744b892a135070cdab7c
StevenLu <luxin763323387@163.com>
2023-02-26 22:21:46 +0800

    feat: 服务端发送与接收

---
+++ src/main/java/com/cn/lx/bugstack/netty/server/NettyServer.java
@@ -0,0 +1,67 @@
+package com.cn.lx.bugstack.netty.server;
+
+import com.cn.lx.bugstack.netty.server.handler.MyServerHandler;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.LineBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+
+import java.nio.charset.Charset;
+
+/**
+ * @author StevenLu
+ * @date 2023/2/26 8:41 下午
+ */
+public class NettyServer {
+
+    public static void main(String[] args) {
+        NettyServer nettyServer = new NettyServer();
+        nettyServer.bind(9000);
+    }
+
+    private void bind(int port) {
+
+        // 老板就是 bossGroup 负责接口， workerGroup 负责处理
+        // bossGroup 表示监听端口，accept 新连接的线程组
+        NioEventLoopGroup boss = new NioEventLoopGroup();
+        // workerGroup 表示处理每一条连接的数据读写的线程组
+        NioEventLoopGroup work = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(boss, work)
+                    //表示系统用于临时存放已完成三次握手的请求的队列的最大长度，如果连接建立频繁，服务器处理创建新连接较慢，可以适当调大这个参数
+                    .option(ChannelOption.SO_BACKLOG, 1024)
+                    // 指定IO模型
+                    .channel(NioServerSocketChannel.class)
+                    // 我们调用childHandler()方法，给这个引导类创建一个ChannelInitializer，这里主要就是定义后续每条连接的数据读写
+                    // childHandler() 为 workGroup 中的 Channel 绑定处理器，同上
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            /* 解码器 */
+                            // 基于换行符号
+                            ch.pipeline().addLast(new LineBasedFrameDecoder(1024));
+                            // 字节码转String 继承了MessageToMessageDecoder
+                            ch.pipeline().addLast(new StringDecoder(Charset.forName("GBK")));
+                            //在管道中添加我们自己的接收数据实现方法
+                            ch.pipeline().addLast(new MyServerHandler());
+                        }
+                    });
+            ChannelFuture f = serverBootstrap.bind(port).sync();
+            System.out.println("netty-server started");
+            f.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            boss.shutdownGracefully();
+            work.shutdownGracefully();
+        }
+
+    }
+}
---
+++ src/main/java/com/cn/lx/bugstack/netty/server/handler/MyServerHandler.java
@@ -0,0 +1,55 @@
+package com.cn.lx.bugstack.netty.server.handler;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.socket.SocketChannel;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @author StevenLu
+ * @date 2023/2/26 9:00 下午
+ */
+public class MyServerHandler extends SimpleChannelInboundHandler {
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        // netty的SocketChannel
+        SocketChannel channel = (SocketChannel) ctx.channel();
+        System.out.println("链接报告信息：有一客户端链接到本服务端");
+        System.out.println("链接报告IP:" + channel.localAddress().getHostString());
+        System.out.println("链接报告Port:" + channel.localAddress().getHostString());
+        System.out.println("链接报告完毕");
+        // 通知简历客户端链接 建立成功
+        String str = "通知客户端链接建立成功" + " " + new Date() + " " + channel.localAddress().getHostString() + "\r\n";
+        ByteBuf buf = Unpooled.buffer(str.getBytes().length);
+        buf.writeBytes(str.getBytes("GBK"));
+        ctx.writeAndFlush(buf);
+    }
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
+//        ByteBuf buf = (ByteBuf) msg;
+//        // readableBytes获取可用字节数量，初始化字节数组
+//        byte[] bytes = new byte[buf.readableBytes()];
+//        // 将buf写入到bytes 容器中
+//        buf.readBytes(bytes);
+//        System.out.println(new Date() + "接收到消息：" );
+//        System.out.println(new String(bytes, Charset.forName("GBK")) );
+        System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()) + " 接收到消息：" + msg);
+
+        // 通知客户端发送消息成功
+        String str = "服务端收到：" + new Date() + " " + msg + "\r\n";
+        ByteBuf buf = Unpooled.buffer(str.getBytes().length);
+        buf.writeBytes(str.getBytes("GBK"));
+        ctx.writeAndFlush(buf);
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("客户端断开链接" + ctx.channel().localAddress().toString());
+    }
+}
